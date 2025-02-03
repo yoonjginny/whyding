e9f6f2da6c9800f7639a4862263d90e28a8d447c
bgt30 <rhkdfla910676@gmail.com>
2025-02-04 08:22:41 +0900

    refactor: Configure CORS with environment-based allowed origins

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # CORS settings
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = os.getenv('CORS_ALLOWED_ORIGINS').split(',')
 CORS_ALLOW_CREDENTIALS = True
 
 # REST Framework settings
